fd6696efcf3fe327ff39146dd5c7fdce640a1089
normanvnfc <norman@venuerific.com>
2021-10-12 15:46:06 +0700

    Create some modal

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,11 @@ console.log("Load script.js");
 
 // Instantiating the global app object
 var app = {};
+var getStartedModal = new bootstrap.Modal(document.getElementById("getStarted"), {});
+var vacStationModal = new bootstrap.Modal(document.getElementById("stationModal"), {});
+var vacBusesModal = new bootstrap.Modal(document.getElementById("busesModal"), {});
+document.onreadystatechange = function () {
+  //getStartedModal.show();
+  //vacStationModal.show();
+  vacBusesModal.show();
+};
